40af20ccc00e367399661096374d48423d8575b6
unknown <Gorbachev-AA@Gorbachev-PC.orenmfc.ru>
2013-08-08 16:20:57 +0600

    frontend with bootstrap

--- camsViewer/settings.py
+++ camsViewer/settings.py
@@ -73,7 +73,7 @@ MEDIA_URL = '/media/'
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = 'static/'
+STATIC_ROOT = ''
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
@@ -85,7 +85,9 @@ STATICFILES_DIRS = (
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
     os.path.join(PROJECT_ROOT, 'third_party', 'components'),
+    os.path.join(PROJECT_ROOT, 'static'),
 )
+
 
 # List of finder classes that know how to find static files in
 # various locations.
@@ -145,7 +147,7 @@ INSTALLED_APPS = (
     'django.contrib.admin',
     ##########
     'south',
-    'djangobower',
+    # 'djangobower',
     #########
     'apps.fs',
     'apps.main',
